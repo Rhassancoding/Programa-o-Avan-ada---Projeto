6f0efa446564c0274e884ccf61a7d103b58e2bc7
Rhassan Rodrigues Santos <rhassanrodrigues6@gmail.com>
2021-06-11 20:06:56 -0300

    Delete GAMEQUIZ 2.0/GameQuiz.X directory

--- GAMEQUIZ 2.0/GameQuiz.X/src/main/java/com/mycompany/gamequiz/x/Temas.java
+++
@@ -1,8 +0,0 @@
-
-package com.mycompany.gamequiz.x;
-public class Temas extends Perguntas{
-    
-   void Assuntos(){System.out.println("\n-------Esses são os Assuntos-------\n1-Tecnologia\n2-Games\n3-Filmes");
-      }
-  
-}
